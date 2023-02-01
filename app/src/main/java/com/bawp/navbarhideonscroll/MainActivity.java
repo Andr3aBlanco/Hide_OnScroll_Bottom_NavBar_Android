de062c3992560482cf5f303a87b51b7160fbf8b5
Andrea Blanco <56937994+Andr3aBlanco@users.noreply.github.com>
2023-01-31 22:03:58 -0800

    NavBar click goes to fragment
    4 empty fragments - click on icon woks

--- app/src/main/java/com/bawp/navbarhideonscroll/MainActivity.java
+++ app/src/main/java/com/bawp/navbarhideonscroll/MainActivity.java
@@ -1,14 +1,67 @@
 package com.bawp.navbarhideonscroll;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.bawp.navbarhideonscroll.databinding.ActivityMainBinding;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //bind
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+//        setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot()); //error with layout -> doesn't have getRoot method - returned back to original
+
+        //opens for the first time Home Fragment
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+
+                case R.id.menu_home:
+                    replaceFragment(new HomeFragment());
+                    break;
+
+                case R.id.menu_orders:
+                    replaceFragment(new OrdersFragment());
+
+                    break;
+
+                case R.id.menu_profile:
+                    replaceFragment(new ProfileFragment());
+
+                    break;
+
+                case R.id.menu_stats:
+                    replaceFragment(new StatsFragment());
+
+                    break;
+
+            }
+
+            return true;
+        });
+    }
+
+    //Method to replace the FrameLayout with the fragment
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.commit();
+
     }
 }
